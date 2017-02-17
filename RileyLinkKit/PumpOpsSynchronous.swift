f7f8b16cbeffea9486e5e11d2059750860823fe1
Jaim Zuber <jaim@sharpfivesoftware.com>
2017-02-17 16:12:46 -0600

    remove page check

--- RileyLinkKit/PumpOpsSynchronous.swift
+++ RileyLinkKit/PumpOpsSynchronous.swift
@@ -571,18 +571,18 @@ class PumpOpsSynchronous {
                     timestamp.timeZone = pump.timeZone
 
                     if let date = timestamp.date?.addingTimeInterval(timeAdjustmentInterval) {
-                        if date.timeIntervalSince(startDate) < -eventTimestampDeltaAllowance {
-                            NSLog("Found event at (%@) to be more than %@s before startDate(%@)", date as NSDate, String(describing: eventTimestampDeltaAllowance), startDate as NSDate);
-                            //break pages
-                        } else if date.timeIntervalSince(timeCursor) > eventTimestampDeltaAllowance {
-                            NSLog("Found event (%@) out of order in history. Ending history fetch.", date as NSDate)
-                            break pages
-                        } else {
+//                        if date.timeIntervalSince(startDate) < -eventTimestampDeltaAllowance {
+//                            NSLog("Found event at (%@) to be more than %@s before startDate(%@)", date as NSDate, String(describing: eventTimestampDeltaAllowance), startDate as NSDate);
+//                            //break pages
+//                        } else if date.timeIntervalSince(timeCursor) > eventTimestampDeltaAllowance {
+//                            NSLog("Found event (%@) out of order in history. Ending history fetch.", date as NSDate)
+//                            break pages
+//                        } else {
                             if (date.compare(startDate) != .orderedAscending) {
                                 timeCursor = date
                             }
                             events.insert(TimestampedHistoryEvent(pumpEvent: event, date: date), at: 0)
-                        }
+//                        }
                     }
                 }
 
